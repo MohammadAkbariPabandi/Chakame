787025ab95389072e5616e71443225efa9f106dc
Mohammad <remap0095@yahoo.com>
2024-10-07 12:25:28 +0330

    responsive to recourse

--- themes/chakame/page-ResourceParent.php
+++ themes/chakame/page-ResourceParent.php
@@ -16,8 +16,19 @@ get_header();
     ?>
 </header>
 
+<section class="container resourceParentContent">
+    <div class="resourceParentContent_body" >
+        <?php
+        $post_id = get_the_ID(); // Get the current post ID
+        $content = get_post_field('post_content', $post_id); // Retrieve the raw content
+        $content = apply_filters('the_content', $content); // Apply the content filters to format it
+        echo $content; // Output the content
+        ?>
+    </div>
+</section>
+
 <section class="container resourceParent__childe">
-    <h4>پروژه هایی که با این طبفه بندی اجرا شده</h4>
+    <h4>پروژه هایی که با این طبقه بندی اجرا شده</h4>
     <div class="row">
         <?php
         // Include WordPress core files
@@ -35,32 +46,34 @@ get_header();
             foreach ($posts as $recentPost) {
                 $hero = get_field('hero', $recentPost->ID);
                 $summary = get_field('summary', $recentPost->ID);
-                echo '<div class="col-md-6 col-12 horizontalCard LazyLoad">';
-                echo '<div class="horizontalCard__image">';
-                if($hero){
-                    echo '<img src="' . $hero['url'] . '">';
-                }
-                echo '</div>';
-                echo '<div class="horizontalCard__content">';
-                if($recentPost->post_title){
-                    echo '<h5 class="horizontalCard__content_text">' . esc_html($recentPost->post_title) .'</h5>';
-                }
-                if($summary){
-                    echo '<p>'.$summary.'</p>';
-                }
-                echo '<div class="horizontalCard__content_link">';
-                echo '<a class="text-decoration-none " href="'. get_permalink($recentPost).'" style="color:#1651A5">ادامه
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="12" viewBox="0 0 17 12"
-                            fill="#1651A5" style="margin-left:8px; margin-bottom: 5px;">
-                            <path
-                                d="M1 5.25C0.585786 5.25 0.25 5.58579 0.25 6C0.25 6.41421 0.585786 6.75 1 6.75V5.25ZM16.5303 6.53033C16.8232 6.23744 16.8232 5.76256 16.5303 5.46967L11.7574 0.696699C11.4645 0.403806 10.9896 0.403806 10.6967 0.696699C10.4038 0.989593 10.4038 1.46447 10.6967 1.75736L14.9393 6L10.6967 10.2426C10.4038 10.5355 10.4038 11.0104 10.6967 11.3033C10.9896 11.5962 11.4645 11.5962 11.7574 11.3033L16.5303 6.53033ZM1 6.75H16V5.25H1V6.75Z"
-                                fill="#1651A5" />
-                        </svg>
-                    </span>
-                </a>';
-                echo '</div>';
-                echo '</div>';
+                echo '<div class="col-md-6 col-12 horizontalCard">';
+                    echo '<div class = "row">';
+                        echo '<div class="col-md-5 horizontalCard__image">';
+                            if($hero){
+                                echo '<img src="' . $hero['url'] . '">';
+                            }
+                        echo '</div>';
+                        echo '<div class="col-md-7 horizontalCard__content">';
+                            if($recentPost->post_title){
+                                echo '<h5 class="horizontalCard__content_text">' . esc_html($recentPost->post_title) .'</h5>';
+                            }
+                            if($summary){
+                                echo '<p>'.$summary.'</p>';
+                            }
+                            echo '<div class="horizontalCard__content_link">';
+                                echo '<a class="text-decoration-none " href="'. get_permalink($recentPost).'" style="color:#1651A5">ادامه
+                                    <span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="12" viewBox="0 0 17 12"
+                                            fill="#1651A5" style="margin-left:8px; margin-bottom: 5px;">
+                                            <path
+                                                d="M1 5.25C0.585786 5.25 0.25 5.58579 0.25 6C0.25 6.41421 0.585786 6.75 1 6.75V5.25ZM16.5303 6.53033C16.8232 6.23744 16.8232 5.76256 16.5303 5.46967L11.7574 0.696699C11.4645 0.403806 10.9896 0.403806 10.6967 0.696699C10.4038 0.989593 10.4038 1.46447 10.6967 1.75736L14.9393 6L10.6967 10.2426C10.4038 10.5355 10.4038 11.0104 10.6967 11.3033C10.9896 11.5962 11.4645 11.5962 11.7574 11.3033L16.5303 6.53033ZM1 6.75H16V5.25H1V6.75Z"
+                                                fill="#1651A5" />
+                                        </svg>
+                                    </span>
+                                </a>';
+                            echo '</div>';
+                        echo '</div>';
+                    echo '</div>';   
                 echo '</div>';
             }
         
